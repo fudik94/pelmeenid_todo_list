4ad5683e92aac56b22ff243ebf2b4576c147f390
Siuzanna Nevar <siuzanna.nevar@ut.ee>
2026-03-09 13:17:44 +0200

    added clear_completed_tasks() function

--- todo.py
+++ todo.py
@@ -1,4 +1,3 @@
-from mark_complete import mark_complete
 import json
 import os
 
@@ -42,8 +41,9 @@ def main_menu():
         print("\n=== TODO LIST MANAGER ===")
         print("1. Add Task")
         print("2. View Tasks")
-        print("3. Exit")
-        print("4. Mark Task Complete")
+        print("3. Delete Task")
+        print("4. Exit")
+        print("5. Clear Completed Tasks")
         choice = input("Choose an option: ")
 
         if choice == "1":
@@ -57,14 +57,16 @@ def main_menu():
                 status = "Done" if t["completed"] else "Pending"
                 print(f"{t['id']}: {t['description']} [{t['priority']}] - {status}")
         elif choice == "3":
-            print("Exiting...")
-            break
-        elif choice == "4":
             try:
-                task_id = int(input("Enter task ID to mark complete: "))
-                mark_complete(tasks, task_id, save_tasks)
+                task_id = int(input("Enter task ID to delete: "))
+                delete_task(task_id)
             except ValueError:
                 print("Invalid ID. Please enter a number.")
+        elif choice == "4":
+            print("Exiting...")
+            break
+        elif choice == "5":
+            clear_completed_tasks()
         else:
             print("Invalid option. Try again.")
 
@@ -82,12 +84,28 @@ def set_priority(task_id, new_priority):
             return
     print(f"Task with id {task_id} not found.")
 
-if __name__ == "__main__":
-    add_task("Task 1")            # Medium by default
-    add_task("Task 2", "High")    # High
-    add_task("Task 3", "Low")     # Low
+#=== Delete a task ===
+def delete_task(task_id):
+    for task in tasks:
+        if task['id'] == task_id:
+            tasks.remove(task)
+            save_tasks(tasks)
+            print(f"Task {task_id} deleted.")
+            return
+    print(f"Task with id {task_id} is not found.")
 
-    set_priority(1, "Low")        # Task 1 → Low
-    set_priority(2, "Medium")     # Task 2 → Medium
-    set_priority(4, "High")       # ID 4 doesn't exist
-    set_priority(2, "Urgent")     # Invalid priority
+# === Clear completed tasks ===
+def clear_completed_tasks():
+    any_deleted = False # Flag to check if any tasks were deleted
+    for task in tasks[:]: # Iterate over a copy of the list
+        if task['completed']:
+            tasks.remove(task)
+            any_deleted = True
+    save_tasks(tasks)
+    if any_deleted:
+        print("All completed tasks have been cleared.")
+    else:
+        print("No completed tasks to clear.")
+
+if __name__ == "__main__":
+    main_menu() #start menu
